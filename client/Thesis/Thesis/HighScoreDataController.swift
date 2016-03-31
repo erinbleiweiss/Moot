9fe2e5e318d8347d60649dfee7b20a8fd1f9b974
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-03-31 17:24:42 -0500

    high score table displays data correctly

--- client/Thesis/Thesis/HighScoreDataController.swift
+++ client/Thesis/Thesis/HighScoreDataController.swift
@@ -15,6 +15,39 @@ import SwiftyJSON
 
 class HighScoreDataController: GenericGameController{
 
-
+    var highScores: [[String: String]] = []
+    let num_scores_to_retrieve = 4
+    
+    func getHighScores(completionHandler: (responseObject: JSON?, error: NSError?) -> ()) {
+        let url: String = hostname + rest_prefix + "/get_high_scores"
+        Alamofire.request(.GET, url, parameters: ["num_scores": num_scores_to_retrieve])
+            .responseJSON { (_, _, result) in
+                switch result {
+                case .Success(let data):
+                    let json = JSON(data)
+                    print(json["status"])
+                    
+                    for (item, subJson):(String, JSON) in json{
+                        if (item == "scores"){
+                            print(subJson)
+                            self.createScores(subJson)
+                        }
+                    }
+                    completionHandler(responseObject: json, error: result.error as? NSError)
+                case .Failure(_):
+                    NSLog("Request failed with error: \(result.error)")
+                }
+        }
+    }
+    
+    func createScores(scores: JSON){
+        for (_, subJson):(String, JSON) in scores {
+            let name = subJson["name"].string
+            let score = String(subJson["score"].int!)
+            highScores.append([name!: score])
+        }
+    }
+    
+    
 
 }
--- client/Thesis/Thesis/HighScoreViewController.swift
+++ client/Thesis/Thesis/HighScoreViewController.swift
@@ -12,11 +12,22 @@ class HighScoreViewController: MootViewController, UITableViewDelegate, UITableV
 
     @IBOutlet weak var tableView: UITableView!
     
+    let controller: HighScoreDataController
+    required init?(coder aDecoder: NSCoder) {
+        controller = HighScoreDataController()
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        self.controller.getHighScores(){ responseObject, error in
+            self.tableView.reloadData()
+            print(self.controller.highScores)
+        }
     
     }
 
@@ -32,12 +43,29 @@ class HighScoreViewController: MootViewController, UITableViewDelegate, UITableV
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return 1
+        return self.controller.highScores.count
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("HighScoreCell", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("HighScoreCell", forIndexPath: indexPath)
+        
+        let scoreInfo = self.controller.highScores[indexPath.row]
+        let halfWidth = cell.frame.width / 2
+        let margin = halfWidth * 0.05
+        
+        let nameLabel = UILabel(frame: CGRectMake(0, 0, halfWidth - margin, cell.frame.height))
+        nameLabel.textAlignment = .Right
+        let scoreLabel = UILabel(frame: CGRectMake(halfWidth + margin, 0, halfWidth - margin, cell.frame.height))
+        scoreLabel.textAlignment = .Left
+        
+        for scoreItem in scoreInfo{
+            nameLabel.text = "\(scoreItem.0): "
+            scoreLabel.text = scoreItem.1
+        }
+        
+        cell.addSubview(nameLabel)
+        cell.addSubview(scoreLabel)
         
         return cell
     }
