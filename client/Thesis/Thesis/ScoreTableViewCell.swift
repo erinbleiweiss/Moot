c227e495c8ac92f0f101d3ffe1e9a669f1246b34
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-04-06 03:25:16 -0500

    alignment improvement

--- client/Thesis/Thesis/ScoreTableViewCell.swift
+++ client/Thesis/Thesis/ScoreTableViewCell.swift
@@ -19,7 +19,8 @@ class ScoreTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        let halfWidth = self.frame.width / 2
+        let halfWidth = ScreenWidth / 2
+        let scoreWidth = ScreenWidth * 0.275
         let margin = halfWidth * 0.05
         let indent = 20 * scale
         
@@ -29,8 +30,9 @@ class ScoreTableViewCell: UITableViewCell {
         self.nameLabel = UILabel(frame: CGRectMake(indent, 0, halfWidth - margin, self.frame.height))
         self.nameLabel.textAlignment = .Center
         
-        self.scoreLabel = UILabel(frame: CGRectMake(halfWidth, 0, halfWidth - margin, self.frame.height))
+        self.scoreLabel = UILabel(frame: CGRectMake(ScreenWidth - scoreWidth - margin, 0, scoreWidth - margin, self.frame.height))
         self.scoreLabel.textAlignment = .Left
+        
 
         self.nameLabel.font = UIFont(name: (self.nameLabel.font?.familyName)!, size: 20 * scale)
         self.scoreLabel.font = UIFont(name: (self.scoreLabel.font?.familyName)!, size: 20 * scale)
